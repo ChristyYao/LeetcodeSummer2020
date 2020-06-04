0706ddd63ff8df7eb4641c8a3db9a645d7cdd76f
christyyao <yao.christy@gmail.com>
2020-06-03 19:55:05 -0700

    mysqrt comments

--- mySqrt.py
+++ mySqrt.py
@@ -1,3 +1,6 @@
+# O(log(n)) time: not too sure but theres halving and sqrting going on each iteration
+# O(1) space: only need curr variable
+
 class Solution:
     def mySqrt(self, x: int) -> int:
         print(x)
