fb5fde2701c6db33375d3283a83c6e745e370404
christyyao <yao.christy@gmail.com>
2020-06-18 21:27:45 -0700

    lowestCommonAncestor

---
+++ lowestCommonAncestor.py
@@ -0,0 +1,23 @@
+# O(log(n)) time: worst case p and q are the most left or the most right two nodes, 
+#                 then the time is halving during each recursion 
+# O(1) space: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if root.val == p.val or root.val == q.val:
+            return root
+        
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
