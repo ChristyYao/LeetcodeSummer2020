0aff2b72d8d333a4acb142d1061ba04d78acbb8c
christyyao <yao.christy@gmail.com>
2020-06-19 20:04:19 -0700

    productExceptSelf

--- productExceptSelf.py
+++ productExceptSelf.py
@@ -1,14 +1,26 @@
-import numpy as np
+# O(n) time: iterate through nums once
+# O(1) space: store l, r variables
+
+# import numpy as np
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        # print(nums)
-        # print(nums[1:]+nums[:1])
         res = [1] * len(nums)
-        for i in range(len(nums)-1):
-            arr = nums[i:] + nums[:i]
-            print(arr)
-            res = np.multiply(res, arr)
-            print(res)
+        l = r = 1
+        
+        for i in range(len(nums)):
+            res[i] *= l
+            res[len(nums) - 1 - i] *= r
+            l *= nums[i]
+            r *= nums[len(nums) - 1 - i]
+        
+        return res
+        
+#         res = [1] * len(nums)
+#         for i in range(len(nums)-1):
+#             arr = nums[i:] + nums[:i]
+#             print(arr)
+#             res = np.multiply(res, arr)
+#             print(res)
             
-        return np.roll(res, -1)
+#         return np.roll(res, -1)
